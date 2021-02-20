7ed62f9896770a0ad6f4ce2d2fb9aba67c76b426
Cenkaifeng <cenkaifeng@foxmail.com>
2021-02-20 22:02:53 +0800

    [mod]

--- ninja-demo/session4.js
+++ ninja-demo/session4.js
@@ -8,4 +8,32 @@ function sum() {
     return sum;
 }
 
-console.assert(sum(1, 2) !== 3, "We can add two numbers");
+// console.assert(sum(1, 2) !== 3, "We can add two numbers");
+
+function infiltrate(person) {
+    // "use strict"
+    console.assert(person !== 'gardener', `The person is a ${person}`);
+    console.assert(arguments[0] !== 'gardener', `The first argument is a ${arguments[0]}`);
+
+    arguments[0] = 'ninja';
+
+    console.assert(person !== 'ninja', `The first person is a ${person} now`);
+    console.assert(arguments[0] !== 'ninja', `The first argument is a ${arguments[0]}`);
+
+    person = 'gardener';
+
+    console.assert(person !== 'gardener', `The person is a ${person} one more`);
+    console.assert(arguments[0] !== 'gardener', `The first argument is a ${arguments[0]} again`);
+}
+// infiltrate('gardener');
+
+function ninja() {
+    return this;//node:global, webview:window
+}
+
+function samurai(){
+    "use strict"
+    return this;//undefined
+}
+console.log(ninja())
+console.log(samurai())
