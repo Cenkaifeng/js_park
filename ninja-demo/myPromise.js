7751b8b9f39421b8c782963ac8167bbd0f142bb1
Cenkaifeng <cenkaifeng@foxmail.com>
2021-02-24 08:39:49 +0800

    [mod] add promise

--- ninja-demo/myPromise.js
+++ ninja-demo/myPromise.js
@@ -1,4 +1,4 @@
-// 手写Promise
+// Promise/A+ 规范 手写Promise
 // 定义三种状态
 
 const PENDING = "pending";
@@ -12,15 +12,19 @@ function MyPromise(callback) {
     seft.value = null;// 成功返回值
     self.error = null;// 失败原因
     self.status = PENDING;
-    self.onFulfilled = null;// 成功回调 resolve
-    self.onRejected = null;
+    // self.onFulfilled = null;// 成功回调 resolve
+    // self.onRejected = null;
+
+    self.onFulfilledCallbacks = [];
+    self.onRejectedCallbacks = []; // 注册回调改数组用于后续链式调用
 
     function resolve(value) {
         if(self.status === PENDING) {
             // 利用宏任务机制让方法在then后执行 
             setTimeout(()=> {
                 self.value = value;
-                self.onFulfilled(self.value)// resolve 成功回调
+                // self.onFulfilled(self.value)// resolve 成功回调
+                self.onFulfilledCallbacks.forEach( callback => callback(self.value));// 遍历数组执行回调
             })            
         }
     }
@@ -30,17 +34,27 @@ function MyPromise(callback) {
             setTimeout(()=> {
                 self.status = REJECTED;//执行报错后改状态
                 self.error = error;
-                self.onRejected(self.error);
+                // self.onRejected(self.error);
+                self.onRejectedCallbacks.forEach(callback => callback(self.error));
             })            
         }
-
     }
     callback(resolve, reject);
 }
 MyPromise.prototype.then = function(onFulfilled, onRejected) {
-    // 给promise实例注册成功和失败回调
-    this.onFulfilled = onFulfilled;
-    this.onRejected = onRejected;
+    if (this.status === PENDING) {
+        // 给promise实例注册成功和失败回调
+        // this.onFulfilled = onFulfilled;
+        // this.onRejected = onRejected;
+        this.onFulfilledCallbacks.push(onFulfilled)
+        this.onRejectedCallbacks.push(onRejected)
+    } else if ( this.status === FULFILLED) {
+        // 如果状态是fulfilled 直接执行成功回调
+        onFulfilled(this.value);
+    } else {
+        onRejected(this.error);
+    }
+    return this; // 支持链式
 }
 
 module.exports = MyPromise
