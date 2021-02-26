44427615c58ac96b41ec16bf8b84358f7be87628
cenkaifeng <cenkaifeng@foxmail.com>
2021-02-26 09:52:03 +0800

    [mod] add something about promise

--- ninja-demo/session6.js
+++ ninja-demo/session6.js
@@ -89,18 +89,18 @@ const throwErrorIterator = ThrowError();
 
 // Promise
 
-const ninjaPromise = new Promise( (resolve, reject) => {
-    // resolve("Hattori");
-    reject("An error resolving a promise")
-});
+// const ninjaPromise = new Promise( (resolve, reject) => {
+//     // resolve("Hattori");
+//     reject("An error resolving a promise")
+// });
 
-ninjaPromise.catch( e => {
-    console.log('看看有没有')
-}).then( ninja => {
-    console.log('xx',ninja)
-}, err => {
-    console.log(err)
-})
+// ninjaPromise.catch( e => {
+//     console.log('看看有没有')
+// }).then( ninja => {
+//     console.log('xx',ninja)
+// }, err => {
+//     console.log(err)
+// })
 
 
 
@@ -110,3 +110,106 @@ ninjaPromise.catch( e => {
 //         return this
 //     }
 // }
+
+
+// 查看Promise执行顺序
+
+// console.log('start')
+var ninjaDelayedPromise = new Promise( (resolve, reject) => {
+    // console.log('ninjaDelayedPromise executor');
+    setTimeout( () => {
+        // console.log('Resolving ninjaDelayedPromise');
+        resolve("heihei");
+    },500)
+})
+
+// console.log(ninjaDelayedPromise)
+
+ninjaDelayedPromise.then( ninja => {
+    // console.log('then ' + ninja)
+})
+
+const ninjaImmediatePromise = new Promise( (resolve, reject) => {
+    // console.log('ninjaImmediatePromise executor');
+    resolve('yep!');
+})
+
+ninjaImmediatePromise.then( ninja => {
+    // console.log(ninja)
+})
+
+// console.log('end')
+
+// start
+// ninjaDelayedPromise executor
+// [Promise]
+// ninjaImmediatePromise excutor
+// end
+// yep
+// Resolving ninjaDelayedPromise
+// then heihei
+
+
+// 显式拒绝
+
+const rejectPromise = new Promise((resolve, reject) =>{
+    reject('Explicitly reject a promise');
+})
+
+rejectPromise.then(()=> console.log("Happy Path"), error => {
+    // console.log('lalala', error)
+})
+
+// 隐式拒绝
+
+const rejectPromise2 = new Promise((resolve, reject) => {
+    undeclaredVariable ++;
+})
+
+rejectPromise2.then( () => console.log("Happy Path")
+// , rej => console.log('reject',rej)
+).catch(error => {
+    // console.log('catch',error)
+})
+
+// 真实demo Promise 封装 xhr
+
+function getJSON(url) {
+    return new Promise((resolve, reject) => {
+
+        const request = new XMLHttpRequest();
+
+        request.open('GET', url);
+        request.onload = function() {
+            try {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.response));
+                } else {
+                    reject(this.status + " " + this.statusText);
+                }
+            } catch (e) {
+                reject(e.message);
+            }
+        }
+
+        request.onerror = function() {
+            reject(this.status + " " + this.statusText);
+        }
+
+        request.send();
+    })
+}
+
+// 调用方式
+// getJSON("xxxx").then( resolve => {
+//     console.log(resolve);
+// }, err => {
+//     console.log(err);
+// })
+
+//链式调用
+
+// getJSON("xxx")
+// .then( ninjas => getJSON(ninjas))
+// .then( xxx => getJSON(xxx))
+// .catch( error => console.log(error));
